1dfcda272c094907ffb0efeb6c4dcc00ceb04f12
Brett Cohen <brettgmf@Bretts-MacBook-Air.local>
2014-05-09 15:57:58 -0500

    created first do while loop that counted from 0 to 100

--- do_while.php
+++ do_while.php
@@ -1,11 +1,20 @@
 <?php
 
-Create a file named do_while.php in your exercises repository.
+// Create a file named do_while.php in your exercises repository.
 
-After each step, commit your changes with a descriptive message and push to origin.
+// After each step, commit your changes with a descriptive message and push to origin.
 
-Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a newline.
+// Create a do-while loop that will count by 2's starting with 0 and ending at 100. 
+// Follow each number with a newline.
 
-Alter your loop to count backwards by 5's from 100 to -10.
+// Alter your loop to count backwards by 5's from 100 to -10.
 
-Create a do-while loop that starts at 2, and displays the result $a * $a on each line while $a is less than 1,000,000. Output should equal:
+// Create a do-while loop that starts at 2, 
+// and displays the result $a * $a on each line while $a is less than 1,000,000. Output should equal:
+
+$a = 0;
+
+do {
+    echo "$a\n";
+    $a += 2;
+} while ($a <=100);
