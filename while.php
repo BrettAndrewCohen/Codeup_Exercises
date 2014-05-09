9b8726a5d6f2e8e9160c402d01100629eeee25ae
Brett Cohen <brettgmf@Bretts-MacBook-Air.local>
2014-05-09 15:13:54 -0500

    while loops

---
+++ while.php
@@ -0,0 +1,18 @@
+<?php
+
+// Create a file in your exercises direcotry named while.php. 
+// Complete the following code to meet requirements in the comments.
+
+// Create a variable $test equal to 5.
+
+// Create a while loop that checks $test <= 15 and increment $test by 1 inside the loop. 
+// Before incrementing $test, output each iteration (echo $test with newline) and display results.
+
+$test = 5;
+
+while ($test <= 15 ) {
+	echo "$test\n";
+	$test++;
+}
+
+
