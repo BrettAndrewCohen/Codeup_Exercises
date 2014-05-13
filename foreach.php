91799b09fece02387a6e77d03f119d21bc2b4af9
Brett Cohen <brettgmf@Bretts-MacBook-Air.local>
2014-05-13 14:54:43 -0500

    finished foreach loop exercise

--- foreach.php
+++ foreach.php
@@ -1,2 +1,44 @@
 <?php
 
+// Create a file named foreach.php in you exercises directory. Commit each step and push to GitHub.
+
+// Construct a loop that iterates through each value and outputs its type as either integer, float, boolean, array, null, or string.
+
+// Construct a loop that iterates through each value and outputs only values with a type that is scalar.
+
+// Create a loop that will echo out every value, including those nested in arrays. Output should look like this.
+
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach($things as $thing){
+	echo "{$thing}" . PHP_EOL;
+}
+
+// foreach($things as $thing) {
+// 	if(is_scalar($thing)){
+// 		echo "$thing" . PHP_EOL;
+// 	}
+// }
+
+// foreach ($things as $thing){
+// 	if(is_int($thing)){
+// 		echo "{$thing} is an Integer" . PHP_EOL;
+// 	}
+// 	elseif(is_float($thing)){
+// 		echo "{$thing} is a Float" . PHP_EOL;
+// 	}
+// 	elseif(is_bool($thing)){
+// 		echo "{$thing} is a Boolean" . PHP_EOL;
+// 	}
+// 	elseif(is_array($thing)){
+// 		print_r ($thing);
+// 	}
+// 	elseif(is_null($thing)){
+// 		echo "{$thing} is Null" . PHP_EOL;
+// 	}
+// 	elseif(is_string($thing)){
+// 		echo "{$thing} is a String" . PHP_EOL;
+// 	}
+// }
+
