53ce535264e7d7686fa9956e0e549205bfee3e8e
vagrant <vagrant@codeup-trusty>
2014-05-14 16:13:25 +0000

    created two additional for loops with break and continue commands

--- break_continue.php
+++ break_continue.php
@@ -7,10 +7,30 @@
 
 // Create another for loop that counts from 1 to 100, but stops after 10 using break.
 
+// The first example prints all numbers from 1 to 100 and and notes even numbers
+
 for ($i = 1; $i <= 100; $i++) {
 	echo $i . PHP_EOL;
 	if ($i % 2 == 1) {
 		continue;
 	}
 	echo "^The number above is even!" . PHP_EOL;
+}
+
+// The second example prints all even numbers
+
+for ($i = 1; $i <= 100; $i++){
+	if ($i % 2 == 1){
+		continue;
+	}
+	echo $i . PHP_EOL;
+}
+
+// The last example prints all numbers from 1 to 10. 
+
+for ($i = 1; $i <= 100; $i++) {
+	echo $i . PHP_EOL;
+	if ($i == 10) {
+		break;
+	}
 }
